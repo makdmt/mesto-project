09d6fe53839afd400d756205566f8a4f46f99395
Dmitry Makarov <makdmt@gmail.com>
2024-04-23 13:45:18 +0300

    fix (app) auth config

--- config.js
+++ config.js
@@ -1,2 +1,5 @@
-export const API_URL = 'http://api.dmt.nomoredomainswork.ru'
-export const USER_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjI2NjRhN2Y5MDAzYmY0YzI1YTdkMzYiLCJpYXQiOjE3MTM3OTIyMTUsImV4cCI6MTcxNDM5NzAxNX0.kXWg-ObnYc2qFq4bETAbCRX_o-70oNlpgMv1BrbOwOE'
+// export const API_URL = 'http://api.dmt.nomoredomainswork.ru/'
+// export const USER_TOKEN = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjI2NjRhN2Y5MDAzYmY0YzI1YTdkMzYiLCJpYXQiOjE3MTM3OTIyMTUsImV4cCI6MTcxNDM5NzAxNX0.kXWg-ObnYc2qFq4bETAbCRX_o-70oNlpgMv1BrbOwOE'
+
+export const API_URL = 'https://nomoreparties.co/v1/plus-cohort-18/'
+export const USER_TOKEN = 'd95e8e0f-5717-4536-87da-f10af028f83a'
